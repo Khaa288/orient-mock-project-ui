4dbe925f128f4b7880e4d732dbd7a2bc6ab18145
ORIENTSOFTWARE\khang.huynh <khanghuynh.orient@gmail.com>
2025-01-16 17:46:39 +0700

    feat: add home module

---
+++ src/features/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { CoreModule } from '../../core/core.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    CoreModule,
+    HomeRoutingModule
+  ],
+  exports: [HomeComponent]
+})
+export class HomeModule { }
